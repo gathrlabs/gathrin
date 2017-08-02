cd10c669f8faed6e07b845c803a10352dcee9f8e
gathrlabs <tomscerri@gathrlabs.com.au>
2017-08-02 20:46:06 +1000

    Adding more modules to dashboard: phone, calendar

---
+++ frontend/src/app/dashboard/calendar/components/Calendar.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="calendar">
+    <h3>You have reached the Calendar component</h3>
+  </div>
+</template>
+<script>
+  export default {
+    name: 'Calendar'
+  }
+</script>
+<style scoped>
+
+</style>
